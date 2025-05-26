2df1defc2867e30a955e0275dc9e6cc0ffe26b20
VIEIRA Ana Clara (Student) <ana.vieira@ext.vallourec.com>
2025-05-26 18:01:19 -0300

    Primeiro commit da página inicial

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const azulNavegacao = Color(0xFF0C3EC2);
+  static const verdeAgua = Color(0xFFB0D0D3);
+  static const lilasClaro = Color(0xFFECE7F9);
+  static const roxo = Color(0xFF7A64BE);
+  static const laranja = Color(0xFFEB6E38);
+  static const amarelo = Color(0xFFF7C64D);
+  static const fundoApp = Color(0xFFEDEDED);
+}
